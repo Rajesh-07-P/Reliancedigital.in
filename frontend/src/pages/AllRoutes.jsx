f1286b7db18987d50d6e751cfd6d9b2d2259abb3
Rajesh-P-07 <rajesh.pachika@gmail.com>
2023-02-23 22:43:22 +0530

    added main carousel

--- frontend/src/pages/AllRoutes.jsx
+++ frontend/src/pages/AllRoutes.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Home'
 
 const AllRoutes = () => {
   return (
     <>
     <Routes>
-        
+      <Route path="/" element={<Home/>}/>
     </Routes>
     </>
   )
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import SimpleSlider from "../components/carousels/topcarousel/SimpleSlider";
+import styles from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <div>
+      <SimpleSlider />
+      <div className={styles.boatoffers}>
+        <h2 >Special Offers on Boat</h2>
+        <div >View all</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
