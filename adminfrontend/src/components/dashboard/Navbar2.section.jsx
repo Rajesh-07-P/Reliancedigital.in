2aa48e98949ebc6c4a5a29cd9770797dcda0b698
Kamal Gupta <agdigitalsmith@gmail.com>
2023-02-26 12:43:22 +0530

    add

---
+++ adminfrontend/src/components/dashboard/Navbar2.section.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from '../../styles/Navbar.module.css'
+const Navbar2section = () => {
+  return (
+    <div className={styles.main2}>
+        <p className={styles.p2}>Orders</p>
+        <p className={styles.p2}>Products</p>
+        <p className={styles.p2}>Create Products</p>
+        <p className={styles.p2}>User's Data</p>
+    </div>
+  )
+}
+
+export default Navbar2section
